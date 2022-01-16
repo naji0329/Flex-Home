7d6b87ec9022311bebf125682fed0c49577ffadd
HungNV <nvhung.dev@gmail.com>
2022-01-16 09:31:07 +0700

    update config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,8 +38,6 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-                 'host' => '127.0.0.1',
-                 'port' => 6001,
                 'scheme' => 'https',
             ],
         ],
